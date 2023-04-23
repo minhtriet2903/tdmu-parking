6e4e87526ed78e03ce7dc6dcd99a675f46698f04
minhtriet2903 <minhtriet20012001@gmail.com>
2023-04-23 23:42:22 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: false,
+});
+
+const nextConfig = {
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
   },
+  reactStrictMode: true,
+  output: "export",
 };
+
+module.exports = withBundleAnalyzer(nextConfig);
+
+// module.exports = {
+//   reactStrictMode: true,
+//   images: {
+//     unoptimized: true,
+//   },
+// };
